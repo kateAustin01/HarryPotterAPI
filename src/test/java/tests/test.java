2ddbbf8e9b0fcb25353cc214da009335b9863cd5
Feri723 <54251953+Feri723@users.noreply.github.com>
2020-07-07 13:15:21 -0500

    Feride test case

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -1,11 +1,21 @@
 package tests;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utilities.ConfigurationReader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class test {
     /*
@@ -27,7 +37,65 @@ public class test {
 
     @Test
     public void Verify_SortingHat() {
-        Response response = given().pathParam("id","sortingHat").when().get("/{id}");
-        System.out.println(response.prettyPrint());
+//
+        Response response = given().pathParam("Mahriban", "sortingHat").when().get("/{Mahriban}");
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json; charset=utf-8",response.contentType());
+        List<String> houses = new ArrayList<>(Arrays.asList("\"Gryffindor\"", "\"Ravenclaw\"", "\"Slytherin\"", "\"Hufflepuff\""));
+        assertTrue(houses.contains(response.getBody().asString()));
+    }
+
+    /*
+    Verify bad key
+1.
+Send a get request to
+/characters
+. Request includes :
+•Header
+Accept
+ with value
+application/json
+•
+Query param
+key
+ with value
+invalid
+2.
+Verify status code
+401
+, content type
+application/json; charset=utf-8
+3.
+Verify response status line include message
+Unauthorized
+4.
+Verify that response body says
+"error":
+"API
+Key
+Not
+Found"
+     */
+    @Test
+    public void verify_Bad_Key(){
+        given().accept(ContentType.JSON).
+                when().queryParam("key", "invalid").
+                get("/characters").prettyPeek()
+                .then().assertThat().statusCode(401).
+                contentType("application/json; charset=utf-8").
+                statusLine(containsString("Unauthorized")).
+                body("error", is("API Key Not Found"));
+    }
+
+    @Test
+    public void verify_no_Key(){
+        given().accept(ContentType.JSON).get("/characters")
+                .then().assertThat().statusCode(409).contentType("application/json; charset=utf-8")
+                .statusLine(containsString("Conflict"))
+                .body("error", is("Must pass API key for request"));
+    }
+    @Test
+    public void  verify void Number_Of_Characters(){
+
     }
 }
