1888b763fae4b2309155f8f6926fed71a268ce49
aktoty@live.com <aktoty@live.com>
2020-07-07 11:23:49 -0500

    first testcase

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -4,8 +4,14 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utilities.ConfigurationReader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class test {
     /*
@@ -27,7 +33,22 @@ public class test {
 
     @Test
     public void Verify_SortingHat() {
-        Response response = given().pathParam("id","sortingHat").when().get("/{id}");
-        System.out.println(response.prettyPrint());
+
+        Response response = given().pathParam("Mahriban", "sortingHat").when().get("/{Mahriban}");
+        response.prettyPrint();
+        String name = response.asString();
+        List<String> houses = new ArrayList<>(Arrays.asList("Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"));
+        assertTrue(houses.contains(name));
+
+
+
+
+
     }
+    /*
+    1. Send a get request to /sortingHat. Request includes :
+2. Verify status code 200, content type application/json; charset=utf-8
+3. Verify that response body contains one of the following houses:
+"Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"
+     */
 }
