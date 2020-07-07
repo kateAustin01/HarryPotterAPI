953ebb694c887d906a8f5b54b2a6acb3a5263c2d
mahribana <gmahriban89@gmail.com>
2020-07-07 13:20:12 -0500

    Duzeldi

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -12,15 +12,11 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.*;
-<<<<<<< HEAD
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-=======
-import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
->>>>>>> bb90f816e9f9754e7b20e20db9d8d9369aa9dbee
+
 
 public class test {
     /*
@@ -42,50 +38,16 @@ public class test {
 
     @Test
     public void Verify_SortingHat() {
-<<<<<<< HEAD
-//
-=======
 
->>>>>>> bb90f816e9f9754e7b20e20db9d8d9369aa9dbee
         Response response = given().pathParam("Mahriban", "sortingHat").when().get("/{Mahriban}");
         assertEquals(200,response.statusCode());
         assertEquals("application/json; charset=utf-8",response.contentType());
         List<String> houses = new ArrayList<>(Arrays.asList("\"Gryffindor\"", "\"Ravenclaw\"", "\"Slytherin\"", "\"Hufflepuff\""));
         assertTrue(houses.contains(response.getBody().asString()));
-<<<<<<< HEAD
+
     }
 
-    /*
-    Verify bad key
-1.
-Send a get request to
-/characters
-. Request includes :
-•Header
-Accept
- with value
-application/json
-•
-Query param
-key
- with value
-invalid
-2.
-Verify status code
-401
-, content type
-application/json; charset=utf-8
-3.
-Verify response status line include message
-Unauthorized
-4.
-Verify that response body says
-"error":
-"API
-Key
-Not
-Found"
-     */
+
     @Test
     public void verify_Bad_Key(){
         given().accept(ContentType.JSON).
@@ -104,33 +66,9 @@ Found"
                 .statusLine(containsString("Conflict"))
                 .body("error", is("Must pass API key for request"));
     }
-    @Test
-    public void  verify void Number_Of_Characters(){
-=======
->>>>>>> bb90f816e9f9754e7b20e20db9d8d9369aa9dbee
 
-    }
 
-    /*
-    * Verify bad key
-    * 1.Send a get request to /characters.
-    * Request includes :
-    * •Header Accept with value application/json
-    * •Query param key with value invalid.
-    * Verify status code 401, content type application/json; charset=utf-8
-    * 3.Verify response status line include message Unauthorized.
-    * Verify that response body says"error":"APIKeyNotFound"
-     */
-    @Test
-    public void verify_Bad_Key(){
 
-        given().accept(ContentType.JSON).
-                when().queryParam("key", "invalid").
-                get("/characters").prettyPeek()
-                .then().assertThat().statusCode(401).
-                                    contentType("application/json; charset=utf-8").
-                                    statusLine(containsString("Unauthorized")).
-                                    body("error", is("API Key Not Found"));
-    }
+
 
 }
